d2ebfca3d5f9dcc6cdd51b8066757ac45de52ee0
wujiajia12345 <3230571270@qq.com>
2023-08-22 17:31:54 +0800

    .\python_repos.py--pygal

--- python_repos.py
+++ python_repos.py
@@ -7,14 +7,21 @@ r = requests.get(URL)
 print('Status code: ', r.status_code)
 
 response_dict = r.json()
-print("Total repositories: ", response_d
+print("Total repositories: ", response_dict['total_count'])
 
-
+repo_dicts = response_dict['items']
 
 names, stars = [], []
 for repo_dict in repo_dicts:
-    names.append(repo_dict['name'])uuuuuuu
+    names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
 
+my_style = LS('#333366',  base_style = LCS)
+chart = pygal.Bar(style = my_style, x_label_rotation = 45, show_lengend = False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
 
 
